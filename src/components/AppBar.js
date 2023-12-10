f8d601daa9f78b6012ec641d41f6898d7903b537
nuwanjith-malaka <nuwanjithm@gmail.com>
2023-12-10 14:21:28 +0530

    Added a logo

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import Link from '@mui/material/Link';
 
 // start -> MUI dark and light mode toggling
 import { useTheme } from '@mui/material/styles';
@@ -56,31 +57,28 @@ function ResponsiveAppBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  
 
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
       <CssBaseline />
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component={RouterLink} 
-            to="/"
-            sx={{
+        <Link href="/">
+          <Box
+            component="img"
+            sx={{ height: 25,
               mr: 2,
               display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
               color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
+              textDecoration: 'none', }}
+            alt="Logo"
+            src={"https://malakas3.s3.amazonaws.com/pizzeria/logo/pizzeria-black.png"}
+          />
+        </Link>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -141,7 +139,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            logo
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Copyright(props) {
       <Typography sx={{ mt: 8, mb: 4 }} variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
         <Link color="inherit" href="https://mui.com/">
-          Your Website
+          Pizzeria
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
