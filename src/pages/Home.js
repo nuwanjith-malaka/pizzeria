705247e4f5be454ab2a5737cf9fd2f050f238997
nuwanjith-malaka <nuwanjithm@gmail.com>
2024-05-07 16:12:27 +0530

    removed getUserToken function being calling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -102,7 +102,7 @@ function Home() {
     const queryParams = new URLSearchParams(window.location.search);
     console.log('printing URLSearchParams', queryParams)
     
-    Promise.all([getPizzas(), getExtras(), getUserTokens(queryParams)])
+    Promise.all([getPizzas(), getExtras()])
       .then(([pizzas, extras, tokens]) => {
         console.log('adding pizzas and extras to the state')
         setState({...State, Extras:extras, Pizzas:pizzas, User:tokens});
@@ -132,4 +132,4 @@ export default Home;
 
 // curl -X GET https://pizzzzeria.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=qgklh1tp03tvqav39sjaafct2&response_type=code&scope=openid+profile+aws.cognito.signin.user.admin+email+phone&redirect_uri=https%3A%2F%2Fpizzzzeria.com&state=abcdefg"
 
-// curl -X POST https://pizzzzeria.auth.us-east-1.amazoncognito.com/oauth2/token -H "Content-Type"="application/x-www-form-urlencoded" -H "Authorization"="Basic cWdrbGgxdHAwM3R2cWF2MzlzamFhZmN0MjoxYXJybDA3dTdpYmI0Y3I4dmxvN3N0ZW50cm00Z2k5NmNoZmsxb2g1NDRtYXF2amxhMXI2" -d "grant_type=authorization_code&client_id=qgklh1tp03tvqav39sjaafct2&client_secret=1arrl07u7ibb4cr8vlo7stentrm4gi96chfk1oh544maqvjla1r6&code=d7825675-d258-4d54-a84b-56db10b32d93&redirect_uri=https://pizzzzeria.com"
+// curl -X POST https://pizzzzeria.auth.us-east-1.amazoncognito.com/oauth2/token -H "Content-Type"="application/x-www-form-urlencoded" -H "Authorization"="Basic cWdrbGgxdHAwM3R2cWF2MzlzamFhZmN0MjoxYXJybDA3dTdpYmI0Y3I4dmxvN3N0ZW50cm00Z2k5NmNoZmsxb2g1NDRtYXF2amxhMXI2" -d "grant_type=authorization_code&client_id=qgklh1tp03tvqav39sjaafct2&client_secret=1arrl07u7ibb4cr8vlo7stentrm4gi96chfk1oh544maqvjla1r6&code=b0587ff9-fa4a-43c6-a5f3-9ca5d8e00e6a&state&redirect_uri=https://pizzzzeria.com"
